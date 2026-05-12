a508f67cca9f0e0b4bf5395be682673ff98e5eb0
lodovikoa <lodoviko@gmail.com>
2026-05-12 09:15:22 -0300

    feat:Aulas

--- src/app/features/template-driven-forms/sync-validators/sync-validators.component.ts
+++ src/app/features/template-driven-forms/sync-validators/sync-validators.component.ts
@@ -1,9 +1,10 @@
+import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-sync-validators',
-  imports: [FormsModule],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './sync-validators.component.html',
   styleUrl: './sync-validators.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
