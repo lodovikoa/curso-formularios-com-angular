fe414375c8b40904355f44ba9a17845f83ab3275
lodovikoa <lodoviko@gmail.com>
2026-06-15 18:43:37 -0300

    feat: Reactive Forms - Validações

--- src/app/features/reactive-forms/1.form-control/form-control.component.ts
+++ src/app/features/reactive-forms/1.form-control/form-control.component.ts
@@ -1,10 +1,12 @@
+import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-control',
   imports: [
     ReactiveFormsModule,
+    JsonPipe,
   ],
   templateUrl: './form-control.component.html',
   styleUrl: './form-control.component.css',
@@ -12,6 +14,10 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 })
 export class FormControlComponent {
 
-  protected control = new FormControl();
+  protected nameControl = new FormControl( "", { nonNullable: true, validators: [Validators.required, Validators.minLength(3) ] } );
+
+  protected reset() {
+    this.nameControl.reset();
+  }
 
 }
